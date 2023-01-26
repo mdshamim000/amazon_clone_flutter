5c27bd990e359dcd6566667a4eac5e522285dcc5
Shamim <md_shamim_cs@outlook.com>
2023-01-26 18:54:07 +0800

    created server, init js

--- lib/pages/auth/screens/authscreen.dart
+++ lib/pages/auth/screens/authscreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-import '../../../consts/cust_button.dart';
+import '../../../res/widgets/cust_button.dart';
 import '../../../consts/global_var.dart';
 import '../../../res/widgets/custom_textfield.dart';
 
--- lib/res/widgets/cust_button.dart
+++ lib/res/widgets/cust_button.dart
@@ -14,7 +14,7 @@ class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: Text(
+      child:  Text(
         text,
         style: TextStyle(
           color: color == null ? Colors.white : Colors.black,
